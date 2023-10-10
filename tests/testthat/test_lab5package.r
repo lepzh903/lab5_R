a55e9ddb9881109a2740028c7c8a9996da7a9997
lepzh903 <lepeng0401@gmail.com>
2023-10-10 09:35:22 +0200

    add code chunk in vignettes

--- tests/testthat/test_lab5package.r
+++ tests/testthat/test_lab5package.r
@@ -8,6 +8,7 @@ test_that("getonecitydata() method works", {
   kolada_mod <- kolada$new('Linkoping')
   kolada_data <- kolada_mod$getonecitydata()
   expect_true(is.data.frame(kolada_data))
+  expect_true(names(kolada_data)[2]=='Linkoping')
   expect_equal(round(kolada_data[,2][c(3,17)],3),c(34.155,38.775))
 })
 
@@ -15,6 +16,7 @@ test_that("getallcitydata() method works", {
   kolada_mod <- kolada$new('Uppsala')
   kolada_data <- kolada_mod$getallcitydata()
   expect_true(is.data.frame(kolada_data))
+  expect_true(all(names(kolada_data)==c("period","Stockholm","Gothenburg","Lund","Uppsala","Linkoping")))
   expect_equal(round(kolada_data[,5][c(3,17)],3),c(33.619,37.325))
 })
 
